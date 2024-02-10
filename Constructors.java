04d905561fa345cc72aba705ccf5fdbe37466b4c
DhirendraKumar001 <ankitkumar200545@gmail.com>
2024-02-10 22:11:57 +0530

    added new code

---
+++ Constructors.java
@@ -0,0 +1,39 @@
+public class Constructors{// Constructors Function.
+    public static void main(String[] args) {
+        Fruit f1 = new Fruit();
+        f1.name = "mango";
+        f1.color = "red";
+        f1.quantity = 30;
+        System.out.println(f1.name);
+        
+
+        Fruit f2 = new Fruit(f1);
+        System.out.println(f2.quantity);
+
+
+    }
+}
+
+class Fruit{
+    String name;
+    String color;
+    int quantity;
+
+    Fruit(Fruit f1){//copy Constructor
+        this.name = f1.name;
+        this.color = f1.color;
+        this.quantity = f1.quantity;
+    }
+
+    Fruit (String name){// parameterized constructor.
+          this.name = name;
+        
+    }
+
+    Fruit() {   // Non-parameterized constructor.
+        System.out.println("Yellow");
+
+    }
+}
+
+
